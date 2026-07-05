5d4cf10c7032a8eef018b5e3c1de8ddeab88045a
yusufrjb <yusufrajabiyah653@gmail.com>
2026-07-05 17:11:06 +0700

    vps2

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: 'dashboardaq-web',
+      cwd: '/home/yusuf/Monitoring-Kualitas-Udara-AQindex',
+      script: 'bun',
+      args: 'run start',
+      env: {
+        PORT: 3000,
+        PYTHON_PATH: '/home/yusuf/Monitoring-Kualitas-Udara-AQindex/ml_model/venv/bin/python3',
+      },
+    },
+    {
+      name: 'dashboardaq-watcher',
+      cwd: '/home/yusuf/Monitoring-Kualitas-Udara-AQindex/ml_model',
+      script: '/home/yusuf/Monitoring-Kualitas-Udara-AQindex/ml_model/venv/bin/python3',
+      args: 'live_forecast_watcher.py',
+    },
+  ],
+};
